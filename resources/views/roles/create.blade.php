dc78ef2a6ceb919e644220667555ec9e8d55795b
Luis Fernando Domingo Gómez <al222010066@gmail.com>
2023-03-22 22:44:46 -0600

    Cambios en logos

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -11,7 +11,7 @@
         <div class="header-body">
           <div class="row align-items-center py-4">
             <div class="col-lg-6 col-7">
-              <h6 class="h2 text-white d-inline-block mb-0">Consultoria</h6>
+              <h6 class="h2 text-white d-inline-block mb-0">UTVT - SHOP</h6>
               <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                 <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                     <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="fas fa-home"></i></a></li>
