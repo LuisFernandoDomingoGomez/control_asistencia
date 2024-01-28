2a137cd8f2e06d2cfa3cc20c0530df2dec2b496b
Luis Fernando Domingo Gómez <al222010066@gmail.com>
2024-01-28 15:09:50 -0600

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mi Página con Tarjeta Semitransparente</title>
+    <title>Bienvenido</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <style>
         body {
